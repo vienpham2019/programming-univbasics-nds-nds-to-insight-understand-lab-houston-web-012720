890921898dcabbecc90ddd2128eae27cec5b6deb
vien pham <vienpham2019@gmail.com>
2019-12-27 03:19:14 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,7 +3,6 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  binding.pry
   # Change the code below to pretty print the nds with pp
   return pp(nds)
 end
